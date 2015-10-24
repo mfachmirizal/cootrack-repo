557221c7330751dbffeccad938f3a708c73a39f8
mfachmirizal <mfachmirizal@gmail.com>
2015-10-24 11:45:33 +0700

    Menulis kerangka Class OpenApiUtils.java

---
+++ src/com/tripad/cootrack/utility/OpenApiUtils.java
@@ -0,0 +1,16 @@
+package com.tripad.cootrack.utility;
+
+import java.util.Map;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+// import org.openbravo.base.exception.OBException;
+// import org.openbravo.client.kernel.BaseActionHandler;
+// import org.openbravo.dal.service.OBDal;
+// import org.openbravo.model.common.order.Order;
+
+public class OpenApiUtils  {
+
+    public OpenApiUtils() {
+    }
+}
