e3e65947dcaefe6d22f984280caa01ec2ceb6c77
mfachmirizal <mfachmirizal@gmail.com>
2015-10-27 13:10:57 +0700

    penambahan OpenApiUtils.java (2)

--- src/com/tripad/cootrack/utility/OpenApiUtils.java
+++ src/com/tripad/cootrack/utility/OpenApiUtils.java
@@ -6,6 +6,9 @@ import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 import java.util.Date;
 import java.lang.Long;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.openbravo.base.exception.OBException;
 // import org.openbravo.base.exception.OBException;
 // import org.openbravo.client.kernel.BaseActionHandler;
 // import org.openbravo.dal.service.OBDal;
@@ -13,14 +16,19 @@ import java.lang.Long;
 
 public class OpenApiUtils  {
     //Nanti static var ini hapus
-	public static final String COOTRACT_USERNAME =  "enduserdahlia";
-	public static final String COOTRACT_PASSWORD =  "123456";
+	public static final String COOTRACK_USERNAME =  "enduserdahlia";
+	public static final String COOTRACK_PASSWORD =  "123456";
+	// static var
+	public static final String COOTRACK_GET_TOKEN_URL = "http://api.gpsoo.net/1/auth/access_token?";
+	
 	
     public OpenApiUtils() {
     }
 	
 	public String getToken() {
-		//http://api.gpsoo.net/1/auth/access_token?account=demobkt&time=1445586322&signature=c4fc3f5175a41e63bc5b33eda62fa1b5
+		String getTokenUrl = COOTRACK_GET_TOKEN_URL+"account="+COOTRACK_USERNAME+"&time="+getUnixTime()+"&signature="+(convertToMd5((convertToMd5(COOTRACK_PASSWORD))+getUnixTime()));
+		//post ke server dan dapatkan json response
+		
 		return null;
 	}
 	
@@ -28,6 +36,37 @@ public class OpenApiUtils  {
 		Date date = new Date();
 		long unixTime = (long) date.getTime()/1000;
 		return unixTime;
+	}
+	
+	public String convertToMd5(String param) {
+		String password = param;
+		MessageDigest md;
+    	try {
+			md = MessageDigest.getInstance("MD5");
+			md.update(password.getBytes());
+		} catch (NoSuchAlgorithmException na) {
+			throw new OBException("Error ! : "+na.getMessage());
+		}
+        
+        byte byteData[] = md.digest();
+ 
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+     
+        //System.out.println("Digest(in hex format):: " + sb.toString());
+        
+        //convert the byte to hex format method 2
+        StringBuffer hexString = new StringBuffer();
+    	for (int i=0;i<byteData.length;i++) {
+    		String hex=Integer.toHexString(0xff & byteData[i]);
+   	     	if(hex.length()==1) hexString.append('0');
+   	     	hexString.append(hex);
+    	}
+    	//System.out.println("Digest(in hex format):: " + hexString.toString());
+		return hexString.toString();
 	}
 	
 	
