11e12469daba63562acfb5f1ea447304246591b0
mfachmirizal <mfachmirizal@gmail.com>
2015-10-31 00:16:35 +0700

    bagian delete belum di implement, pikirkan kembali logic agar semmua yg tidak ada pada server, di database = hapus

--- src/com/tripad/cootrack/handler/RefreshListChildAccount.java
+++ src/com/tripad/cootrack/handler/RefreshListChildAccount.java
@@ -8,6 +8,8 @@ import org.openbravo.base.exception.OBException;
 import org.openbravo.client.kernel.BaseActionHandler;
 
 import com.tripad.cootrack.utility.OpenApiUtils;
+import java.util.ArrayList;
+import java.util.HashMap;
 import org.codehaus.jettison.json.JSONArray;
 import org.hibernate.criterion.Restrictions;
 import org.openbravo.base.provider.OBProvider;
@@ -18,6 +20,8 @@ public class RefreshListChildAccount extends BaseActionHandler {
     
     protected JSONObject execute(Map<String, Object> parameters, String data) {
         String hasil = "";
+        HashMap<String,String> tempDataHash = new HashMap<String , String>();
+        ArrayList<HashMap<String,String>> tempDataServer = new ArrayList<HashMap<String,String>>();
         try {
             OpenApiUtils utils = new OpenApiUtils();
             
@@ -27,12 +31,19 @@ public class RefreshListChildAccount extends BaseActionHandler {
             JSONArray childList = (JSONArray) utils.requestStringListChildAccount().get("children");
             String rslt = "";
             OBCriteria<TmcListChildAcc> tmcListChildAcc = null;
+            OBCriteria<TmcListChildAcc> tmcNotExsListChildAcc = null;
             for (int i = 0; i < childList.length(); i++) {
                 String id = childList.getJSONObject(i).get("id").toString();
                 String name = childList.getJSONObject(i).get("name").toString();
                 String showname = childList.getJSONObject(i).get("showname").toString();
-                
-                 tmcListChildAcc = OBDal.getInstance()
+//                
+//                tempDataHash.put("id", id);
+//                tempDataHash.put("name", name);
+//                tempDataHash.put("showname", showname);
+//                
+//                tempDataServer.add(tempDataHash);
+               
+                tmcListChildAcc = OBDal.getInstance()
                         .createCriteria(TmcListChildAcc.class);
                 tmcListChildAcc.add(Restrictions.eq(TmcListChildAcc.PROPERTY_VALUE, id));
                 
@@ -57,21 +68,23 @@ public class RefreshListChildAccount extends BaseActionHandler {
                 
                 
                 // Bagian delete yg tidak ada di server
-                tmcListChildAcc = OBDal.getInstance()
-                        .createCriteria(TmcListChildAcc.class);
-                tmcListChildAcc.add(Restrictions.ne(TmcListChildAcc.PROPERTY_VALUE, id));
+//                tmcNotExsListChildAcc = OBDal.getInstance()
+//                        .createCriteria(TmcListChildAcc.class);
+//                tmcNotExsListChildAcc.add(Restrictions.not(Restrictions.eq(TmcListChildAcc.PROPERTY_VALUE, id))); //
+//                
+//                //TmcListChildAcc notExistsTmcListChildAcc = ;
+//                if (tmcNotExsListChildAcc.count() > 0) {
+//                    OBDal.getInstance().remove(tmcNotExsListChildAcc.list().get(0));
+//                    OBDal.getInstance().flush();
+//                }
                 
-                //TmcListChildAcc notExistsTmcListChildAcc = ;
-                if (tmcListChildAcc.count() > 0) {
-                OBDal.getInstance().remove(tmcListChildAcc.list().get(0));
-                OBDal.getInstance().flush();
-                }
-                
-                
+               
                 
             }
+            
+            
             OBDal.getInstance().commitAndClose();
-
+            
             //new CustomJsonErrorResponse("5555", "Fatal protocol violation : "+e.getMessage()).getErrResponse();
             JSONObject json = new JSONObject();
             json.put("jawaban", hasil);
