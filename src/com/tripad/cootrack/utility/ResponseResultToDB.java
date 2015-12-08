ece1cdf01e26bb0982181f3d11dffbdee5a29d5e
mfachmirizal <mfachmirizal@gmail.com>
2015-12-08 11:47:26 +0700

    Penambahan Fungsi Rekursif untuk insert Child BP [ Beta] ----- Buat juga Business Category-nya tanpa Hardcoded,

--- src/com/tripad/cootrack/utility/ResponseResultToDB.java
+++ src/com/tripad/cootrack/utility/ResponseResultToDB.java
@@ -92,7 +92,7 @@ public class ResponseResultToDB {
     }
     
     public void validateBPList(JSONObject hasilRetrieve) throws Exception, OBException {
-        ArrayList<String> tempIdDataServer = new ArrayList<String>();
+        /*ArrayList<String> tempIdDataServer = new ArrayList<String>();
         ArrayList<String> tempImeiServer = new ArrayList<String>();
         JSONArray childList = (JSONArray) hasilRetrieve.get("children");
         OpenApiUtils utils = new OpenApiUtils();
@@ -274,6 +274,198 @@ public class ResponseResultToDB {
         }
         
         OBDal.getInstance().commitAndClose();
+        */
+      ArrayList<String> tempIdDataServer = new ArrayList<String>();
+      ArrayList<String> tempImeiServer = new ArrayList<String>();
+      JSONArray childList = (JSONArray) hasilRetrieve.get("children");
+      OpenApiUtils utils = new OpenApiUtils();
+      String rslt = "";
+      OBCriteria<BusinessPartner> tmcListChildAcc = null;
+      // OBCriteria<TmcListChildAcc> tmcNotExsListChildAcc = null;
+      // var mobil
+      JSONObject hasilTarget;
+      JSONArray carList;
+      for (int i = 0; i < childList.length(); i++) {
+          String id = childList.getJSONObject(i).get("id").toString();
+          String name = childList.getJSONObject(i).get("name").toString();
+          String showname = childList.getJSONObject(i).get("showname").toString();
+          
+          tmcListChildAcc = OBDal.getInstance().createCriteria(BusinessPartner.class);
+          tmcListChildAcc.add(Restrictions.eq(BusinessPartner.PROPERTY_TMCOPENAPIIDENT, id));
+          tmcListChildAcc.add(Restrictions.eq(BusinessPartner.PROPERTY_CREATEDBY, COOTRACK_USER));
+          
+          if (tmcListChildAcc.list().isEmpty()) { // bila tidak ada maka insert
+              BusinessPartner newBusinessPartner = OBProvider.getInstance().get(BusinessPartner.class);
+              
+              newBusinessPartner.setActive(true);
+              newBusinessPartner.setTmcOpenapiIdent(id);
+              // newBusinessPartner.setTmcOpenapiUser(name);
+              newBusinessPartner.setSearchKey(name);
+              newBusinessPartner.setName(showname);
+              newBusinessPartner.setConsumptionDays(Long.getLong("0"));
+              newBusinessPartner.setCreditLimit(BigDecimal.ZERO);
+              
+              OBCriteria<Category> bpCrit = OBDal.getInstance().createCriteria(Category.class);
+              bpCrit.add(Restrictions.eq(Category.PROPERTY_SEARCHKEY, "CustomerOpenApi"));
+              if (bpCrit.list().isEmpty()) {
+                  Category newBpCat = OBProvider.getInstance().get(Category.class);
+                  newBpCat.setActive(true);
+                  newBpCat.setName("CustomerOpenApi");
+                  newBpCat.setSearchKey("CustomerOpenApi");
+                  newBpCat.setDescription("Business Partner Category For Customer Open Api");
+                  
+                  OBDal.getInstance().save(newBpCat);
+                  OBDal.getInstance().flush();
+                  
+                  newBusinessPartner.setBusinessPartnerCategory(newBpCat);
+              } else {
+                  newBusinessPartner.setBusinessPartnerCategory(bpCrit.list().get(0));
+              }
+              
+              OBDal.getInstance().save(newBusinessPartner);
+              OBDal.getInstance().flush();
+              
+              // get car list
+              hasilTarget = utils.requestListChildAccount(name);
+              try {
+                  // System.out.println(hasilTarget.toString());
+                  carList = (JSONArray) hasilTarget.get("data");
+                  // loop car
+                  for (int c = 0; c < carList.length(); c++) {
+                      TmcCar newTmcCar = OBProvider.getInstance().get(TmcCar.class);
+                      newTmcCar.setActive(true);
+                      newTmcCar.setBpartner(newBusinessPartner);
+                      newTmcCar.setImei(carList.getJSONObject(c).get("imei").toString());
+                      newTmcCar.setName(carList.getJSONObject(c).get("name").toString());
+                      newTmcCar.setPlateNo(carList.getJSONObject(c).get("number").toString());
+                      newTmcCar.setTelephone(carList.getJSONObject(c).get("phone").toString());
+                      newTmcCar.setTime(Long.valueOf(carList.getJSONObject(c).get("in_time").toString()));
+                      newTmcCar.setOUTTime(Long.valueOf(carList.getJSONObject(c).get("out_time").toString()));
+                      
+                      OBDal.getInstance().save(newTmcCar);
+                      OBDal.getInstance().flush();
+                      // newTmcCar.set
+                  }
+              } catch (JSONException jex) {
+                  // do nothing, data tidak ada
+                  // System.out.println("excpt> "+hasilTarget.toString());
+              }
+              
+          } else { // bila BP SUdah adaa, maka edit
+              
+              // tmcListChildAcc.list().get(0).setTmcOpenapiUser(name);
+              
+              tmcListChildAcc.list().get(0).setSearchKey(name);
+              tmcListChildAcc.list().get(0).setName(showname);
+              
+              OBDal.getInstance().save(tmcListChildAcc.list().get(0));
+              OBDal.getInstance().flush();
+              
+              // edit line
+              hasilTarget = utils.requestListChildAccount(name);
+              // try {
+              
+              carList = (JSONArray) hasilTarget.get("data");
+              
+              for (int c = 0; c < carList.length(); c++) {
+                  // carList.getJSONObject(i).get("imei").toString()
+                  OBCriteria<TmcCar> tmcListCar = OBDal.getInstance().createCriteria(TmcCar.class);
+                  tmcListCar.add(Restrictions.eq(TmcCar.PROPERTY_BPARTNER, tmcListChildAcc.list().get(0)));
+                  tmcListCar.add(Restrictions.eq(TmcCar.PROPERTY_CREATEDBY, COOTRACK_USER));
+                  tmcListCar.add(Restrictions.eq(TmcCar.PROPERTY_IMEI,
+                          carList.getJSONObject(c).get("imei").toString()));
+                  
+                  if (tmcListCar.list().isEmpty()) {
+                      TmcCar newTmcCar = OBProvider.getInstance().get(TmcCar.class);
+                      newTmcCar.setActive(true);
+                      newTmcCar.setBpartner(tmcListChildAcc.list().get(0));
+                      newTmcCar.setImei(carList.getJSONObject(c).get("imei").toString());
+                      newTmcCar.setName(carList.getJSONObject(c).get("name").toString());
+                      newTmcCar.setPlateNo(carList.getJSONObject(c).get("number").toString());
+                      newTmcCar.setTelephone(carList.getJSONObject(c).get("phone").toString());
+                      newTmcCar.setTime(Long.valueOf(carList.getJSONObject(c).get("in_time").toString()));
+                      newTmcCar.setOUTTime(Long.valueOf(carList.getJSONObject(c).get("out_time").toString()));
+                      
+                      OBDal.getInstance().save(newTmcCar);
+                      OBDal.getInstance().flush();
+                  } else {
+                      tmcListCar.list().get(0).setImei(carList.getJSONObject(c).get("imei").toString());
+                      tmcListCar.list().get(0).setName(carList.getJSONObject(c).get("name").toString());
+                      tmcListCar.list().get(0).setPlateNo(carList.getJSONObject(c).get("number").toString());
+                      tmcListCar.list().get(0).setTelephone(carList.getJSONObject(c).get("phone").toString());
+                      tmcListCar.list().get(0)
+                              .setTime(Long.valueOf(carList.getJSONObject(c).get("in_time").toString()));
+                      tmcListCar.list().get(0)
+                              .setOUTTime(Long.valueOf(carList.getJSONObject(c).get("out_time").toString()));
+                      
+                      OBDal.getInstance().save(tmcListCar.list().get(0));
+                      OBDal.getInstance().flush();
+                  }
+                  
+                  tempImeiServer.add(carList.getJSONObject(c).get("imei").toString());
+                  
+              }
+              
+              // adegan menghapus LINE Bila tidak ada yg seragam
+              OBCriteria<TmcCar> tmcListCarRemove = OBDal.getInstance().createCriteria(TmcCar.class);
+              tmcListCarRemove
+                      .add(Restrictions.eq(TmcCar.PROPERTY_BPARTNER, tmcListChildAcc.list().get(0)));
+              tmcListCarRemove.add(Restrictions.eq(TmcCar.PROPERTY_CREATEDBY, COOTRACK_USER));
+              tmcListCarRemove
+                      .add(Restrictions.not(Restrictions.in(TmcCar.PROPERTY_IMEI, tempImeiServer)));
+              
+              for (TmcCar removeRecord : tmcListCarRemove.list()) {
+                  for (TmcDocumentUpdateLine tmcListChildCar : removeRecord.getTmcDocumentUpdateLineList()) {
+                      OBDal.getInstance().remove(tmcListChildCar);
+                      OBDal.getInstance().flush();
+                  }
+                  OBDal.getInstance().remove(removeRecord);
+                  OBDal.getInstance().flush();
+              }
+              
+          }
+          
+          tempIdDataServer.add(id);
+
+          
+          // cek apakah dia punya anak ?
+          try {
+            JSONArray childListChild = (JSONArray) hasilTarget.get("children");
+            validateBPList(hasilTarget);
+          }catch (JSONException jex) {
+            //skip, berarti tidak punya anak
+          }          
+      }//end loop utama
+      
+      
+      // adegan menghapus record yg ada di local tapi tidak ada di server open api ||Jangan LUPA
+      // DELETE LINENYA DULU
+      // header
+      tmcListChildAcc = OBDal.getInstance().createCriteria(BusinessPartner.class);
+      tmcListChildAcc.add(Restrictions
+              .not(Restrictions.in(BusinessPartner.PROPERTY_TMCOPENAPIIDENT, tempIdDataServer))); //
+      tmcListChildAcc.add(Restrictions.eq(BusinessPartner.PROPERTY_CREATEDBY, COOTRACK_USER));
+      tmcListChildAcc.add(Restrictions.eq(BusinessPartner.PROPERTY_ACTIVE, true));
+      OBCriteria<Category> bpCrit = OBDal.getInstance().createCriteria(Category.class);
+      bpCrit.add(Restrictions.eq(Category.PROPERTY_SEARCHKEY, "CustomerOpenApi"));
+      
+      tmcListChildAcc.add(
+              Restrictions.eq(BusinessPartner.PROPERTY_BUSINESSPARTNERCATEGORY, bpCrit.list().get(0)));
+      
+      for (BusinessPartner removeRecord : tmcListChildAcc.list()) {
+          for (TmcCar removeLine : removeRecord.getTmcCarList()) {
+              for (TmcDocumentUpdateLine tmcListChildCar : removeLine.getTmcDocumentUpdateLineList()) {
+                  OBDal.getInstance().remove(tmcListChildCar);
+                  OBDal.getInstance().flush();
+              }
+              OBDal.getInstance().remove(removeLine);
+              OBDal.getInstance().flush();
+          }
+          OBDal.getInstance().remove(removeRecord);
+          OBDal.getInstance().flush();
+      }
+      
+      OBDal.getInstance().commitAndClose();
     }
     
     public void validateCarStatusList(String header_id, JSONObject hasilRetrieve)
